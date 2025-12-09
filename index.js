ae812208bde54087bc1ab2e466c2454ea47ffc12
jamal-uddin-asif <asifzehendmg@gmail.com>
2025-12-10 01:00:50 +0600

    Payment collection added

--- index.js
+++ index.js
@@ -22,12 +22,30 @@ const client = new MongoClient(uri, {
   },
 });
 
+function generateTrackingId() {
+  const prefix = "TRK"; // change this if needed
+  // Step 1: Get timestamp
+  const now = new Date();
+  const timestamp =
+    now.getFullYear().toString() +
+    String(now.getMonth() + 1).padStart(2, "0") +
+    String(now.getDate()).padStart(2, "0") +
+    String(now.getHours()).padStart(2, "0") +
+    String(now.getMinutes()).padStart(2, "0") +
+    String(now.getSeconds()).padStart(2, "0");
+  // Step 2: Generate random digits
+  const random = Math.floor(100000 + Math.random() * 900000); // 6 digits
+  // Step 3: Combine
+  return `${prefix}-${timestamp}-${random}`;
+}
+
 async function run() {
   try {
     const db = client.db("StyleDecor");
     const userCollection = db.collection("Users");
     const servicesCollection = db.collection("Services");
     const bookingCollection = db.collection("Bookings");
+    const paymentsCollection = db.collection("Payments");
 
     // user related api
     app.post("/users", async (req, res) => {
@@ -44,12 +62,16 @@ async function run() {
     });
 
     app.get("/users", async (req, res) => {
-      const { email } = req.query;
+      const { email, role } = req.query;
+      console.log("AllDecorator --->", role);
       const query = {};
       if (email) {
         query.email = email;
       }
-      const result = await userCollection.find().toArray();
+      if (role) {
+        query.role = role;
+      }
+      const result = await userCollection.find(query).toArray();
       res.send(result);
     });
 
@@ -84,8 +106,24 @@ async function run() {
       if (category) {
         query.category = category;
       }
-      console.log(category);
       const cursor = servicesCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/hero-services", async (req, res) => {
+      const projectFields = {
+        _id: 1,
+        serviceName: 1,
+        serviceImg: 1,
+        cost: 1,
+        shortDescription: 1,
+      };
+      const cursor = servicesCollection
+        .find()
+        .project(projectFields)
+        .skip(7)
+        .limit(4);
       const result = await cursor.toArray();
       res.send(result);
     });
@@ -100,13 +138,17 @@ async function run() {
     // Booking related APIs here
     app.post("/bookings", async (req, res) => {
       const bookingInfo = req.body;
+      bookingInfo.trackingId = generateTrackingId();
       const result = await bookingCollection.insertOne(bookingInfo);
       res.send(result);
     });
 
-    app.get("/bookings/:email", async (req, res) => {
-      const { email } = req.params;
-      const query = { customerEmail: email };
+    app.get("/bookings", async (req, res) => {
+      const { email } = req.query;
+      const query = {};
+      if (email) {
+        query.customerEmail = email;
+      }
       const result = await bookingCollection.find(query).toArray();
       res.send(result);
     });
@@ -121,14 +163,14 @@ async function run() {
     // Payment Related APIs
     app.post("/create-checkout-session", async (req, res) => {
       const service = req.body;
-      const amount = parseInt(service.cost * 100)
+      const amount = parseInt(service.cost * 100);
       const session = await stripe.checkout.sessions.create({
         line_items: [
           {
             // Provide the exact Price ID (for example, price_1234) of the product you want to sell
-            price_data:{
-              currency: 'usd',
-              product_data:{
+            price_data: {
+              currency: "usd",
+              product_data: {
                 name: service.serviceName,
                 description: service.shortDescription,
               },
@@ -139,28 +181,60 @@ async function run() {
         ],
         mode: "payment",
         metadata: {
-            serviceId: service.serviceId
+          serviceId: service.serviceId,
+          trackingId: service.trackingId,
+          serviceName: service.serviceName,
+          shortDescription: service.shortDescription,
+          cost: service.cost
         },
-        customer_email:service.customerEmail,
+        customer_email: service.customerEmail,
         success_url: `${process.env.CLIENT_SITE_DOMAIN}/dashboard/payment-success?session_id={CHECKOUT_SESSION_ID}`,
         cancel_url: `${process.env.CLIENT_SITE_DOMAIN}/dashboard/myBookings`,
       });
 
-      res.send({url:session.url})
+      res.send({ url: session.url });
     });
 
-    app.patch('/payment-success', async(req, res)=>{
-      const {sessionId} = req.query;
-      const session = await stripe.checkout.sessions.retrieve(sessionId)
+    app.patch("/payment-success", async (req, res) => {
+      const { sessionId } = req.query;
+      const session = await stripe.checkout.sessions.retrieve(sessionId);
 
-      if(session.payment_status === 'paid'){
-       const id =  session.metadata.serviceId
-       console.log("ServiceId--->", id)
-       const query = {_id: new ObjectId(id)}
-       const filter = await servicesCollection.findOne(query)
-       console.log("Service --->", filter)
+      // prevent dubble insert for useEffect 
+      const query = {transactionId: session.payment_intent}
+      const isExisit = await paymentsCollection.findOne(query)
+      if(isExisit){
+        res.send({message: 'Payment is exist in payment collection'})
+        return
       }
-    })
+
+      if (session.payment_status === "paid") {
+        
+        const paymentInfo = {
+          serviceName: session.metadata.serviceName,
+          trackingId: session.metadata.trackingId,
+          customerEmail: session.customer_email,
+          serviceId: session.metadata.serviceId,
+          transactionId: session.payment_intent,
+          paymentStatus: session.payment_status,
+          cost: session.metadata.cost
+        };  
+
+        const paymentResult = await paymentsCollection.insertOne(paymentInfo)
+        console.log(paymentResult)
+        // update bookingCollection status 
+        const id = session.metadata.serviceId;
+        const query = { _id: new ObjectId(id) };
+        const updateDoc = {
+          $set: {
+            paymentStatus: "Paid",
+            serviceStatus: "Pending",
+            transactionId: session.payment_intent,
+          },
+        };
+        const bookingUpdateResult = await bookingCollection.updateOne(query, updateDoc);
+        res.send({paymentResult, bookingUpdateResult})
+      }
+    });
 
     await client.db("admin").command({ ping: 1 });
     console.log(
